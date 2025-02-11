2f748028a2e78b4d786ddd7e42d131c4f39043a0
Neamtiu Zaris <zneamtiu@gmail.com>
2025-02-11 15:36:15 +0200

    BE - dunno

--- mobile_app_backend/middleware/authenticate_requests.js
+++ mobile_app_backend/middleware/authenticate_requests.js
@@ -7,12 +7,12 @@ function authenticateToken(req, res, next) {
     return res.status(401).json({ error: "Access denied." });
   }
 
-  const token = authHeader.split(" ")[1]; // Extract the token
+  const token = authHeader.split(" ")[1];
 
   try {
     const verified = jwt.verify(token, process.env.JWT_SECRET);
-    req.user = verified; // Attach decoded user to request object
-    next(); // Continue to the next middleware/route
+    req.user = verified;
+    next();
   } catch (err) {
     return res.status(403).json({ error: "Access denied." });
   }
